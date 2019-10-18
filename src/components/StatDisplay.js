5b528bf11d7d78cae35ad1ba6229241f4892f38b
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2019-10-17 21:14:52 -0700

    stats working again

--- src/components/StatDisplay.js
+++ src/components/StatDisplay.js
@@ -1,73 +1,160 @@
 import React from "react";
-import { Button, ListGroup, ListGroupItem } from "reactstrap";
-import { useStoreState, useStoreActions } from "easy-peasy";
+import { Container, Button } from "reactstrap";
+import { useStoreState, useStoreActions, useStore } from "easy-peasy";
 
 export const StatDisplay = () => {
-  return (
-    <div>
-      <div className="nes-container is-dark is-rounded">
-        <ListStats />
-      </div>
-      <div className="nes-container is-dark is-rounded">
-        <ListActions />
-      </div>
-    </div>
-  );
-};
+  const hunger = useStoreState(state => state.stats.hunger);
+  const hygiene = useStoreState(state => state.stats.hygiene);
+  const happiness = useStoreState(state => state.stats.happiness);
+  const power = useStoreState(state => state.stats.power);
+  const intelligence = useStoreState(state => state.stats.intelligence);
+  const discipline = useStoreState(state => state.stats.discipline);
 
-const ListStats = () => {
-  const statList = useStoreState(state => state.stats.statList);
-  return (
-    <div>
-      <ListGroup style={{ listStyle: "none" }}>
-        {statList.map((stat, idx) => (
-          <ListGroupItem key={idx}>
-            {stat.displayName}: {stat.value}
-          </ListGroupItem>
-        ))}
-      </ListGroup>
-    </div>
-  );
-};
-
-// class ListActions extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       toolTip: ""
-//     };
-//   }
-
-//   mouseEnter = action => {
-//     this.setState({ toolTip: action.description });
-//   };
-//   mouseLeave = () => {
-//     this.setState({ toolTip: "" });
-//   };
-
-//   render() {
-const ListActions = props => {
+  const getHungry = useStoreActions(actions => actions.stats.getHungry);
+  const changeTime = useStoreActions(actions => actions.time.changeTime);
   const updateStats = useStoreActions(actions => actions.stats.updateStats);
-  const playerActions = useStoreState(state => state.stats.playerActions);
-  const statList = useStoreState(state => state.stats.statList);
 
   return (
+    <Container>
+      <div className="nes-table-responsive">
+        <table
+          className="nes-table is-bordered is-dark is-centered"
+          width="99%"
+        >
+          <thead>
+            <tr>
+              <th>Health</th>
+              <th>Personality</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <StatusBarLabel value={hunger} label="Hunger" />
+              </td>
+              <td>
+                <StatusBarLabel value={power} label="Power" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <StatusBarLabel value={hygiene} label="Hygiene" />
+              </td>
+              <td>
+                <StatusBarLabel value={intelligence} label="Intelligence" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <StatusBarLabel value={happiness} label="Happiness" />
+              </td>
+              <td>
+                <StatusBarLabel value={discipline} label="Discipline" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div
+        className="nes-container is-dark is-rounded"
+        onClick={() => {
+          getHungry();
+          changeTime();
+        }}
+      >
+        <p>How will you spend your time with your dragon?</p>
+        <Button
+          className="nes-btn is-primary"
+          onClick={() => updateStats("feed")}
+        >
+          Feeding
+        </Button>
+        <Button
+          className="nes-btn is-primary"
+          onClick={() => updateStats("groom")}
+        >
+          Grooming
+        </Button>
+        <Button
+          className="nes-btn is-primary"
+          onClick={() => updateStats("pet")}
+        >
+          Belly Rubs
+        </Button>
+        <Button
+          className="nes-btn is-primary"
+          onClick={() => updateStats("exercise")}
+        >
+          Exercise
+        </Button>
+        <Button
+          className="nes-btn is-primary"
+          onClick={() => updateStats("educate")}
+        >
+          Education
+        </Button>
+        <Button
+          className="nes-btn is-primary"
+          onClick={() => updateStats("training")}
+        >
+          Obedience Training
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+const StatusBarLabel = ({ value, label }) => {
+  return (
     <div>
-      {/* <div style={{ float: "right" }}>{this.state.toolTip}</div> */}
-      <ListGroup style={{ listStyle: "none" }}>
-        {playerActions.map(action => (
-          <ListGroupItem key={action.id}>
-            <Button
-              className="nes-btn is-primary"
-              onClick={() => updateStats(action)}
-              // onMouseEnter={() => this.mouseEnter(action)}
-              // onMouseLeave={() => this.mouseLeave()}
-            >
-              {action.name}
-            </Button>
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      {label}
+      <StatusBar value={value} />
     </div>
   );
+};
+
+const StatusBar = ({ value }) => {
+  let status;
+  if (value >= 75) {
+    status = 1;
+  } else if (value >= 50) {
+    status = 2;
+  } else if (value >= 25) {
+    status = 3;
+  }
+
+  switch (status) {
+    case 1:
+      return (
+        <progress
+          className="nes-progress is-primary"
+          value={value}
+          max="100"
+        ></progress>
+      );
+    case 2:
+      return (
+        <progress
+          className="nes-progress is-success"
+          value={value}
+          max="100"
+        ></progress>
+      );
+    case 3:
+      return (
+        <progress
+          className="nes-progress is-warning"
+          value={value}
+          max="100"
+        ></progress>
+      );
+    default:
+      return (
+        <progress
+          className="nes-progress is-error"
+          value={value}
+          max="100"
+        ></progress>
+      );
+  }
 };
--- src/models/statsModel.js
+++ src/models/statsModel.js
@@ -1,4 +1,4 @@
-import { action } from "easy-peasy";
+import { action, computed } from "easy-peasy";
 import { statList } from "../shared/stats";
 import { ACTIONS } from "../shared/actions";
 
@@ -6,28 +6,90 @@ const statsModel = {
   statList: statList,
   playerActions: ACTIONS,
 
-  getHungry: action(state => {
-    statList[0].value -= 5;
+  hunger: 100,
+  hygiene: 100,
+  happiness: 100,
+  power: 0,
+  intelligence: 0,
+  discipline: 0,
+
+  // minMaxer = computed(state => {
+  //   if (state > 100)  {
+  //     state = 100
+  //   } else if (state < 0) {
+  //     state = 0
+  //   }
+  // }),
+
+  getHungry: action((state, payload) => {
+    state.hunger -= 10;
+    if (state.hunger < 0) {
+      state.hunger = 0;
+      state.happiness -= 10;
+      if (state.happiness < 0) {
+        state.happiness = 0;
+      }
+    }
+    state.hygiene -= 5;
+    if (state.hygiene < 0) {
+      state.hygiene = 0;
+    }
   }),
 
   updateStats: action((state, payload) => {
-    //names of the affected stats
-    const affectedStatArray = Object.keys(payload.affectedStats);
-    //loop through affected stats
-    for (const statToChange of affectedStatArray) {
-      console.log([...state.statList]);
-      for (let originalStat of [...state.statList]) {
-        if (originalStat.name === statToChange) {
-          originalStat.value += payload.affectedStats[statToChange];
-          console.log("orig stat value: " + originalStat.value);
-          console.log("stat to change: " + payload.affectedStats[statToChange]);
-          if (originalStat.value < 0) {
-            originalStat.value = 0;
-          } else if (originalStat.value > 100) {
-            originalStat.value = 100;
-          }
+    switch (payload) {
+      case "feed":
+        state.hunger += 25;
+        if (state.hunger > 110) {
+          state.hunger = 110;
         }
-      }
+        state.happiness += 5;
+        if (state.happiness > 100) {
+          state.happiness = 100;
+        }
+        break;
+      case "groom":
+        state.hygiene = 105;
+        state.happiness += 5;
+        if (state.happiness > 100) {
+          state.happiness = 100;
+        }
+        break;
+      case "pet":
+        state.happiness = 100;
+        break;
+      case "exercise":
+        state.power += 10;
+        if (state.power > 100) {
+          state.power = 100;
+        }
+        state.hygiene -= 10;
+        if (state.hygiene < 0) {
+          state.hygiene = 0;
+        }
+        break;
+      case "educate":
+        state.intelligence += 10;
+        if (state.intelligence > 100) {
+          state.intelligence = 100;
+        }
+        state.happiness -= 10;
+        if (state.happiness < 0) {
+          state.happiness = 0;
+        }
+        break;
+      case "training":
+        state.discipline += 10;
+        if (state.discipline > 100) {
+          state.discipline = 100;
+        }
+        state.happiness -= 10;
+        if (state.happiness < 0) {
+          state.happiness = 0;
+        }
+        break;
+      default:
+        break;
     }
   })
 };
